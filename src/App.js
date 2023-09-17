babb47590bbc81823a5f51b1328b87a2cf0254ca
manuxpath <manuelgarridoserrano120@gmail.com>
2023-09-18 01:16:47 +0200

    Añadida prueba de elementos Reach

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Principal from './components/principal';
 function App() {
   return (
     <div className="App">
-      
+      <Principal />
     </div>
   );
 }
---
+++ src/components/principal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Principal() {
+
+    return(
+        <div className="container" style={{backgroundColor:"red"}}>
+            <h2 style={{color:"black"}}>Prueba</h2>
+        </div>
+    );
+}
+
+export default Principal;
