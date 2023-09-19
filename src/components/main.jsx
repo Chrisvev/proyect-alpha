136943fa91ad166cd9c5926185a613d732e3eefb
chrisvev <christofervv97@gmail.com>
2023-09-19 23:37:44 +0200

    Nuevas cosas

---
+++ src/components/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../stylesheets/main.css';
+import FormLogin from "./FormLogin";
+
+
+function Main (){
+
+
+    return(
+        <div className="container-main">
+
+            <FormLogin />
+
+        </div>
+
+    )
+}
+
+export default Main;
